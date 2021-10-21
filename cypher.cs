99929f6ed9cb24bdbc37225980775db0f938b051
marinova-teddy <68281230+marinova-teddy@users.noreply.github.com>
2021-10-22 00:01:50 +0300

    updated ceaser cypher code - cypher completed

--- cypher.cs
+++ cypher.cs
@@ -12,70 +12,104 @@ public class Program
 		{
 			if (key <= 0 || key >= 27)
 			{
-				Console.Write("Error. Please enter a valid key: ");
+				Console.Write("Error. Please enter a valid key between 1 and 25: ");
 				key = Convert.ToInt32(Console.ReadLine());
 			}
 			else
 				break;
 		}
-
+		
 		if (task == 1)
 			Encryption(key);
 		else
 			Decryption(key);
+		
 	}
 
-	public static bool IsItSmallLetter(char symbol)
+	public static bool IsSmallLetter(char symbol)
 	{
 		if (symbol <= 'z' && symbol >= 'a')
 			return true;
-		else
-			return false;
+		return false;
 	}
 	
-	public static bool IsItCapitalLetter(char symbol)
+	public static bool IsCapitalLetter(char symbol)
 	{
 		if (symbol <= 'Z' && symbol >= 'A')
 			return true;
-		else
-			return false;
+		return false;
+	}
+	
+	public static bool IsDigit(char symbol)
+	{
+		if (symbol <= '9' && symbol >= '0')
+			return true;
+		return false;
 	}
 
 	public static void Encryption(int key)
 	{
 		Console.WriteLine("Enter your message: ");
 		string msg = Console.ReadLine();
-		string encrptdMsg = "";
+		string encMsg = "";
+		
 		for (int i = 0; i < msg.Length; i++)
 		{
-			int currChar = msg[i] + key;
-			if (IsItSmallLetter(msg[i]) && !IsItSmallLetter((char)currChar)) currChar -= 26;
-			if (!IsItSmallLetter(msg[i]) && IsItSmallLetter((char)currChar)) currChar += 26;
-			if (IsItCapitalLetter(msg[i]) && !IsItCapitalLetter((char)currChar)) currChar -= 26;
-			if (!IsItCapitalLetter(msg[i]) && IsItCapitalLetter((char)currChar)) currChar += 26;
-			if (msg[i]==' ') { currChar = 32; Console.WriteLine("SPACEBAR");}
-			encrptdMsg += (char)currChar;
+			int currChar = (int) msg[i] + key;
+			//if spacebar
+			if (msg[i]==' ') {currChar=(int)' ';Console.WriteLine("SPACEBAR");}
+			//if lowercase letter
+			if ( IsSmallLetter(msg[i]) && !IsSmallLetter((char)currChar) ) currChar-=26;
+			//if capital letter
+			if ( IsCapitalLetter(msg[i]) && !IsCapitalLetter((char)currChar) ) currChar-=26;
+			//if digit
+			if ( IsDigit(msg[i]) && !IsDigit((char)currChar) ) {
+				do {
+					currChar-=10;
+				} while (!IsDigit((char)currChar));
+			}
+			//if other symbol
+			while ( (msg[i]!=' ') && !IsSmallLetter(msg[i]) && !IsCapitalLetter(msg[i]) && !IsDigit(msg[i]) &&
+			   ( currChar==(int)' ' || IsSmallLetter((char)currChar) || IsCapitalLetter((char)currChar) || IsDigit((char)currChar)) ) {
+				currChar+=key;
+			}
+				
+			encMsg += (char) currChar;
 		}
 
-		Console.Write("This is your encrypted message: {0}", encrptdMsg);
+		Console.Write("This is your encrypted message: {0}", encMsg);
 	}
 
 	static void Decryption(int key)
 	{
 		Console.WriteLine("Enter your message: ");
-		string encrptdMsg = Console.ReadLine();
-		string msg = "";
-		for (int i = 0; i < encrptdMsg.Length; i++)
+		string msg = Console.ReadLine();
+		string decMsg = "";
+		
+		for (int i = 0; i < msg.Length; i++)
 		{
-			int currChar = encrptdMsg[i] - key;
-			if (IsItSmallLetter(encrptdMsg[i]) && !IsItSmallLetter((char)currChar)) currChar += 26;
-			if (!IsItSmallLetter(encrptdMsg[i]) && IsItSmallLetter((char)currChar)) currChar -= 26;
-			if (IsItCapitalLetter(encrptdMsg[i]) && !IsItCapitalLetter((char)currChar)) currChar += 26;
-			if (!IsItCapitalLetter(encrptdMsg[i]) && IsItCapitalLetter((char)currChar)) currChar -= 26;
-			if (encrptdMsg[i]==' ') { currChar = 32; Console.WriteLine("SPACEBAR");}
-			msg += (char)currChar;
+			int currChar = (int)msg[i] - key;
+			//if spacebar
+			if (msg[i]==' ') {currChar=(int)' ';Console.WriteLine("SPACEBAR");}
+			//if lowercase letter
+			if ( IsSmallLetter(msg[i]) && !IsSmallLetter((char)currChar) ) currChar+=26;
+			//if capital letter
+			if ( IsCapitalLetter(msg[i]) && !IsCapitalLetter((char)currChar) ) currChar+=26;
+			//if digit
+			if ( IsDigit(msg[i]) && !IsDigit((char)currChar) ) {
+				do {
+					currChar+=10;
+				} while (!IsDigit((char)currChar));
+			}
+			//if other symbol
+			while ( (msg[i]!=' ') && !IsSmallLetter(msg[i]) && !IsCapitalLetter(msg[i]) && !IsDigit(msg[i]) &&
+			   ( currChar==(int)' ' || IsSmallLetter((char)currChar) || IsCapitalLetter((char)currChar) || IsDigit((char)currChar)) ) {
+				currChar-=key;
+			}
+			decMsg += (char)currChar;
+			Console.Write((int)msg[i] + " ");
 		}
 
-		Console.WriteLine("Message: {0}", msg);
+		Console.WriteLine("\nMessage: {0}", decMsg);
 	}
 }
