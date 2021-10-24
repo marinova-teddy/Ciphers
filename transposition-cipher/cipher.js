1b9d62a615d560de9cf1ca935066526ee513d24d
Teddy <marinova.teddy@gmail.com>
2021-10-24 17:26:35 +0300

    transposition cipher encryption and decryption funcs
    in cipher.js

---
+++ transposition-cipher/cipher.js
@@ -0,0 +1,33 @@
+function encryption(msg, key) { // working
+    let encMsg = "";
+    let cols = key;
+    let rows = Math.floor(msg.length/key) + 1;
+
+    for (let i=0; i<cols; i++) {
+        for (let j=0; j<rows; j++) {
+            if ((i+key*j)>=msg.length) continue;
+            encMsg+=msg[i+key*j];
+        }
+    }
+
+    return encMsg;
+} 
+
+function decryption(encMsg, key) {
+    let msg = "";
+    let cols = Math.ceil(encMsg.length/key);
+    let rows = key;
+
+    for (let i=0; i<cols; i++) {
+        for (let j=0; j<rows; j++) {
+            if ((i+cols*j)>=encMsg.length) continue;
+            console.log(i+cols*j);
+            msg+=encMsg[i+cols*j];
+        }
+    }
+
+    return msg;
+}
+
+// test: encryption("It is also important that the work goes beyond.", 8)
+// decryption("Isttrbtoa ke  nt yiithgosm eon pt edaohws.lrao ", 8)
