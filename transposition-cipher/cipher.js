ccbd4b089a50a49c0bad964a7f4373a69ecc8e9b
Teddy <marinova.teddy@gmail.com>
2021-11-16 16:56:50 +0200

    cipher corrected
    both encryption and decryption are working now

--- transposition-cipher/cipher.js
+++ transposition-cipher/cipher.js
@@ -1,29 +1,30 @@
-function encryption(msg, key) { // working
+function encryption(msg, key) {
     let encMsg = "";
     let cols = key;
     let rows = Math.floor(msg.length/key) + 1;
 
     for (let i=0; i<cols; i++) {
-        for (let j=0; j<rows; j++) {
-            if ((i+key*j)>=msg.length) continue;
-            encMsg+=msg[i+key*j];
+        for (let j=0;j<rows; j++) {
+            if ((j*cols+i)<msg.length)
+                encMsg+=msg[j*cols+i];
         }
     }
-
     return encMsg;
 } 
 
 function decryption(encMsg, key) {
-    let msg = "";
-    let cols = Math.ceil(encMsg.length/key);
+    let decMsg = "";
+    let cols = Math.floor(encMsg.length/key) + 1;
     let rows = key;
+    let k = encMsg.length - (cols-1)*rows;
+    let n = 0;
 
     for (let i=0; i<cols; i++) {
         for (let j=0; j<rows; j++) {
-            if ((i+cols*j)>=encMsg.length) continue;
-            msg+=encMsg[i+cols*j];
+            if (j<k) decMsg+=encMsg[j*cols+i];
+            else if (i+1!=cols) decMsg+=encMsg[j*cols+i-j+k];
         }
     }
 
-    return msg;
+    return decMsg;
 }
