b07e14252c391d1cdf051795a70136675a689794
Teddy <marinova.teddy@gmail.com>
2021-11-26 13:41:08 +0200

    added multiplicative cipher

---
+++ affine-cipher/AffineCipher/AffineCipher/HelperMethods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace AffineCipher
+{
+    public class GCD
+    {
+        public int Gcd(int a, int b)
+        {
+            if (b > a)
+            {
+                a = a + b;
+                b = a - b;
+                a = a - b;
+            }
+            if (a % b == 0) return b;
+            return Gcd(b, a % b);
+        }
+    }
+}
