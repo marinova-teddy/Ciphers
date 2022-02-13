2fc60e26d2e74d02134122b869b233b5a4dd421f
Teodora Marinova <68281230+marinova-teddy@users.noreply.github.com>
2022-02-13 20:18:01 +0000

    update last code

--- affine-cipher/affine-cipher.cs
+++ affine-cipher/affine-cipher.cs
@@ -1,183 +1,179 @@
 using System;
 
+using System;
+
 namespace AffineCipher
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			Console.WriteLine("Encryption or Decryption? Enter E or D: ");
-			string task = Console.ReadLine();
-			Console.WriteLine("Enter message: ");
-			string msg = Console.ReadLine();
-			Console.WriteLine("Enter key: ");
-			int key = Convert.ToInt32(Console.ReadLine());
-			if (task.Equals("E"))
-				Encryption(msg, key);
-			else if (task.Equals("D"))
-				Decryption(msg, key);
-		}
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Encryption or Decryption? Enter E or D: ");
+string task = Console.ReadLine();
 
-		public static void Encryption(string msg, int key)
-		{
-			//split key and check key for mult.cipher
-			int[] cipherKey = new int[2];
-			cipherKey = HelperMethods.SplitKey(key, msg.Length);
-			//multiplicative cipher
-			string encMsg = MultiplicativeCipher.Encryption(msg, cipherKey[0]);
-			//caesar cipher
-			encMsg = CaesarCipher.Encryption(encMsg, cipherKey[1]);
-			//output encrypted message
-			Console.WriteLine(encMsg);
-		}
+Console.WriteLine("Enter message: ");
+            string msg = Console.ReadLine();
+            Console.WriteLine("Enter key: ");
+            int key = Convert.ToInt32(Console.ReadLine());
 
-		public static void Decryption(string msg, int key)
-		{
-			//split key and check key for mult.cipher
-			int[] cipherKey = new int[2];
-			cipherKey = HelperMethods.SplitKey(key, msg.Length);
-			//caesar cipher
-			string decMsg = CaesarCipher.Decryption(msg, cipherKey[1]);
-			Console.WriteLine(decMsg);
-			//multiplicative cipher
-			decMsg = MultiplicativeCipher.Decryption(decMsg, cipherKey[0]);
-			//output encrypted message
-			Console.WriteLine(decMsg);
-		}
-	}
+if (task.Equals("E"))
+Encryption(msg, key);
+else if (task.Equals("D"))
+Decryption(msg, key);
+        }
+public static void Encryption(string msg, int key)
+{
+            //split key and check key for mult.cipher
+            int[] cipherKey = new int[2];
+            cipherKey = HelperMethods.SplitKey(key, msg.Length);
 
-	public class HelperMethods
-	{
-		public static int Gcd(int a, int b)
-		{
-			if (b > a)
-			{
-				a = a + b;
-				b = a - b;
-				a = a - b;
-			}
+            //multiplicative cipher
+            string encMsg = MultiplicativeCipher.Encryption(msg, cipherKey[0]);
 
-			if (b == 0)
-				return a;
-			return Gcd(b, a % b);
-		}
+            //caesar cipher
+            encMsg = CaesarCipher.Encryption(encMsg, cipherKey[1]);
 
-		public static int[] SplitKey(int key, int len)
-		{
-			const int alph = 26;
-			//multiplicative cipher key
-			int key0 = key / len;
-			while (Gcd(key0, alph) != 1)
-				key0--;
-			//caesar cipher key
-			int key1 = key % len;
-			if (key1 >= alph)
-				key1 %= alph;
-			int[] newKey = {key0, key1};
-			return newKey;
-		}
-		public static Boolean Letter(int a)
-		{
-			if (a>=1 && a<=26) return true;
-			else return false;
-		}
-		public static int CeasSymbolDecr(int sym, int key, int alph)
-		{
-			int decSym = sym - key;
-			while (!Letter(decSym)) 
-				decSym+=alph;
-			
-			return decSym;
-		}
-		public static Boolean WholeNumber(double a)
-		{
-			if (Math.Floor(a)==a) return true;
-			else return false;
-		}
-		public static int MultSymbolDecr(double sym, int key, int alph)
-		{
-			double decSym = (double) sym/key;
-			while (!WholeNumber(decSym)) 
-			{
-				sym+=alph;
-				decSym = (double) sym/key;
-			}
-			
-			return (int) decSym;
-		}
-	}
+            //output encrypted message
+            Console.WriteLine(encMsg);
+}
+public static void Decryption(string msg, int key)
+{
+            //split key and check key for mult.cipher
+            int[] cipherKey = new int[2];
+            cipherKey = HelperMethods.SplitKey(key, msg.Length);
 
-	public class CaesarCipher
-	{
-		public static string Encryption(string msg, int key)
-		{
-			string encMsg = "";
-			const int alph = 26;
-			for (int i = 0; i < msg.Length; i++)
-			{
-				if (msg[i] <= 'z' && msg[i] >= 'a')
-					encMsg += (char)('a' + (((int)msg[i] - 'a') + key) % alph);
-				else if (msg[i] <= 'Z' && msg[i] >= 'A')
-					encMsg += (char)('A' + (((int)msg[i] - 'A') + key) % alph);
-				else
-					encMsg += (char)msg[i];
-			}
-			
-			return encMsg;
-		}
+            //caesar cipher
+            string decMsg = CaesarCipher.Decryption(msg, cipherKey[1]);
 
-		public static string Decryption(string msg, int key)
-		{
-			string decMsg = "";
-			const int alph = 26;
-			for (int i = 0; i < msg.Length; i++)
-			{
-				if (msg[i] <= 'z' && msg[i] >= 'a')
-					decMsg += (char) ('a' + HelperMethods.CeasSymbolDecr(((int)msg[i] - 'a'), key, alph) );
-				else if (msg[i] <= 'Z' && msg[i] >= 'A')
-					decMsg += (char) ('A' + HelperMethods.CeasSymbolDecr(((int)msg[i] - 'A'), key, alph) );
-				else
-					decMsg += (char)msg[i];
-			}
+            //multiplicative cipher
+            decMsg = MultiplicativeCipher.Decryption(decMsg, cipherKey[0]);
 
-			return decMsg;
-		}
-	}
+            //output encrypted message
+            Console.WriteLine(decMsg);
+}
+    }
 
-	public class MultiplicativeCipher
-	{
-		public static string Encryption(string msg, int key)
-		{
-			string encMsg = "";
-			const int alph = 26;
-			for (int i = 0; i < msg.Length; i++)
-			{
-				if (msg[i] <= 'z' && msg[i] >= 'a')
-					encMsg += (char)('a' + ((int)msg[i] - 'a') * key % alph);
-				else if (msg[i] <= 'Z' && msg[i] >= 'A')
-					encMsg += (char)('A' + ((int)msg[i] - 'A') * key % alph);
-				else
-					encMsg += (char)msg[i];
-			}
+    public class HelperMethods
+    {
+        public static int Gcd(int a, int b)
+        {
+            if (b > a)
+            {
+                a = a + b;
+                b = a - b;
+                a = a - b;
+            }
+            if (b == 0) return a;
+            return Gcd(b, a % b);
+        }
+        public static int[] SplitKey(int key, int len)
+        {
+            const int alph = 26;
 
-			return encMsg;
-		}
+            //multiplicative cipher key
+            int key0 = key / len;
+            while (Gcd(key0, alph) != 1)
+                key0--;
 
-		public static string Decryption(string msg, int key)
-		{
-			string decMsg = "";
-			const int alph = 26;
-			for (int i = 0; i < msg.Length; i++)
-			{
-				if (msg[i] <= 'z' && msg[i] >= 'a')
-					decMsg += (char) ('a' + HelperMethods.MultSymbolDecr(((int)msg[i] - 'a'), key, alph) );
-				else if (msg[i] <= 'Z' && msg[i] >= 'A')
-					decMsg += (char) ('A' + HelperMethods.MultSymbolDecr(((int)msg[i] - 'A'), key, alph) );
-				else
-					decMsg += (char)msg[i];
-			}
-		
-			return decMsg;
-		}
-	}
+            //caesar cipher key
+            int key1 = key % len;
+if (key1>=alph)
+key1%=alph;
+
+            int[] newKey = {key0, key1};
+
+            return newKey;
+        }
+/*public static int modInverse(int a, int n)
+{
+int i = n, v = 0, d = 1;
+while (a>0) {
+int t = i/a, x = a;
+a = i % x;
+i = x;
+x = d;
+d = v - t*x;
+v = x;
+}
+v %= n;
+if (v<0) v = (v+n)%n;
+return v;
+}*/
+    }
+
+public class CaesarCipher
+    {
+        public static string Encryption(string msg, int key)
+        {
+string encMsg="";
+const int alph = 26;
+
+for (int i=0; i<msg.Length; i++)
+{
+if (msg[i]<='z' && msg[i]>='a')
+encMsg += (char)('a' + (((int)msg[i] - 'a') + key ) % alph);
+                else if (msg[i]<='Z' && msg[i]>='A')
+encMsg += (char)('A' + (((int)msg[i] - 'A') + key ) % alph);
+else
+                    encMsg += (char)msg[i];
+}
+
+return encMsg;
+        }
+public static string Decryption(string msg, int key)
+{
+string decMsg = "";
+const int alph = 26;
+
+for (int i=0; i<msg.Length; i++)
+{
+if (msg[i]<='z' && msg[i]>='a')
+decMsg += (char)('a' + (((int)msg[i] - 'a') - key ) % alph);
+                else if (msg[i]<='Z' && msg[i]>='A')
+decMsg += (char)('A' + (((int)msg[i] - 'A') - key ) % alph);
+else
+                    decMsg += (char)msg[i];
+}
+
+return decMsg;
+}
+    }
+
+public class MultiplicativeCipher
+    {
+        public static string Encryption(string msg, int key)
+        {
+            string encMsg = "";
+            const int alph = 26;
+
+            for (int i=0; i<msg.Length; i++)
+            {
+                if (msg[i]<='z' && msg[i]>='a')
+encMsg += (char) ('a' + ((int)msg[i] - 'a') * key % alph);
+                else if (msg[i]<='Z' && msg[i]>='A')
+encMsg += (char)('A' + ((int)msg[i] - 'A') * key % alph);
+else
+                    encMsg += (char)msg[i];
+            }
+
+            return encMsg;
+        }
+public static string Decryption(string msg, int key)
+        {
+            string decMsg = "";
+            const int alph = 26;
+
+            for (int i=0; i<msg.Length; i++)
+            {
+                if (msg[i]<='z' && msg[i]>='a')
+decMsg += (char) ('a' + ((int)msg[i] - 'a') / key % alph);
+                else if (msg[i]<='Z' && msg[i]>='A')
+decMsg += (char)('A' + ((int)msg[i] - 'A') / key % alph);
+else
+                    decMsg += (char)msg[i];
+            }
+
+            return decMsg;
+        }
+    }
 }
